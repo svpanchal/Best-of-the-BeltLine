08ee550a088dd996b7dc7204421a0839a13fdd35
Ryan Casalino <ryan.casalino@gmail.com>
2015-09-30 22:03:57 -0400

    adding bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home Page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Best of the BeltLine' });
+});
+
+/* GET About Page */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Best of the BeltLine' });
 });
 
 module.exports = router;
