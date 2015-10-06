902ca77c4131b0f4c74ba3a490a8e26ecc16a605
Ryan Casalino <ryan.casalino@gmail.com>
2015-10-06 12:29:09 -0400

    uncommenting app listen in app.js

--- app.js
+++ app.js
@@ -103,6 +103,6 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-// var port = process.env.PORT || 5000; //select your port or let it pull from your .env file
-// app.listen(port);
-// console.log("listening on " + port + "!");
+var port = process.env.PORT || 5000; //select your port or let it pull from your .env file
+app.listen(port);
+console.log("listening on " + port + "!");
