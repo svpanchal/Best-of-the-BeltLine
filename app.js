131ab29ae80310f8edeecdcdd6135105eca21237
Ryan Casalino <ryan.casalino@gmail.com>
2015-10-07 14:04:12 -0400

    adding sorum branch and having events working (kinda)

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(function(req, res, next){
 //********** Routes ****************************
 app.use('/', routes);
 app.use('/users', users);
-//app.use('/events', events);
+app.use('/events', events);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
