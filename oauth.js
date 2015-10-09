f99dd56ac53762a92ae6f24f1bfdfb99a93daf65
Steven Lytovka <ss_imperator@inbox.ru>
2015-10-09 10:25:36 -0400

    Changed facebook links

--- oauth.js
+++ oauth.js
@@ -2,12 +2,12 @@ var ids = {
 facebook: {
  clientID: '1639789246291135',
  clientSecret: '380b44dfbd372843214909981a6b7759',
- callbackURL: 'http://localhost:3000/auth/facebook/callback'
+ callbackURL: 'https://bestbeltline.herokuapp.com/auth/facebook/callback'
 },
 twitter: {
  consumerKey: '2q57zT1BaVYpHF7WWfTrCO7BN',
  consumerSecret: 'cay6p91CnB7Tc0R1LJNYD8nE9OFikAoY7Py6fVMMICosPbUQ0j',
- callbackURL: 'http://localhost:3000/auth/twitter/callback'
+ callbackURL: 'https://bestbeltline.herokuapp.com/auth/twitter/callback'
 }
 }
 
